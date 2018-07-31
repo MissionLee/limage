13dc493b17d382fb16f4408f7438faef329afbb3
missionleePC <missionli@163.com>
2018-07-31 16:07:56 +0800

    create an empty project

---
+++ src/main/java/org/missionlee/limage/LimageApplication.java
@@ -0,0 +1,12 @@
+package org.missionlee.limage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimageApplication.class, args);
+	}
+}
---
+++ src/test/java/org/missionlee/limage/LimageApplicationTests.java
@@ -0,0 +1,16 @@
+package org.missionlee.limage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LimageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
